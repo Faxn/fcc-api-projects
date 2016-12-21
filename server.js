656ccce23f562d5c8578659a1ce46bc6d27ea29f
Blake Curry <blake.curry@gmx.us>
2016-12-21 14:42:22 -0500

    split off timetamp microservice into express sub-app

--- server.js
+++ server.js
@@ -1,40 +1,12 @@
 var express = require('express')
 var app = express()
+var timestampApp = require("./timestamp.js")
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello app!')
 })
 
-MONTHS = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',  'November', 'December']
-
-
-app.get('/:date', function(req, res) {
-	
-	var d;
-	
-	
-	if(/^\d+$/.test(req.params.date )){
-		date = parseInt(req.params.date);
-		d = new Date(date);
-	} else {
-		d = new Date(req.params.date);
-	}
-	
-	var reply = {};
-	console.log(req.params.date)
-	console.log(d);
-	
-	if(isNaN( d.getTime()) ){
-		reply.unix = null
-		reply.natural = null		
-	} else {
-		reply.unix = d.getTime();
-		reply.natural = MONTHS[d.getMonth()] + " " + d.getDay() + ", " + d.getFullYear();		
-	}	
-	
-	res.json(reply);
-	
-});
+app.use('/timestamp/', timestampApp);
 
 
 app.listen(8080, function () {
---
+++ timestamp.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello timestamp ms!')
+})
+
+MONTHS = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',  'November', 'December']
+
+
+app.get('/:date', function(req, res) {
+	
+	//date object to make from params.date
+	var d;	
+	
+	//If it is a unix timestamp
+	if(/^\d+$/.test(req.params.date )){
+		date = parseInt(req.params.date);
+		d = new Date(date);
+	//If it's something else
+	} else {
+		d = new Date(req.params.date);
+	}
+	
+	var reply = {};
+	
+	//If the date is invalid
+	if(isNaN( d.getTime()) ){
+		reply.unix = null
+		reply.natural = null		
+	//The date is valid build a good response object.
+	} else {
+		reply.unix = d.getTime();
+		reply.natural = MONTHS[d.getMonth()] + " " + d.getDay() + ", " + d.getFullYear();		
+	}	
+	
+	res.json(reply);
+	
+});
+
+//export the timestamp app.
+module.exports = app;
